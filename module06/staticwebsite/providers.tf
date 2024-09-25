d44ca23da1b7ad948e726b785a834ae8a392f37f
DanielMN03 <danielmn03@gmail.com>
2024-09-25 13:45:38 +0200

    staticwebsite

---
+++ module06/staticwebsite/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.2.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "navn-rg-backend"         # Your resource group name
+    storage_account_name = "sanamercavjosk"          # The new storage account name
+    container_name       = "navnstoragecontainer"    # The container name where Terraform state will be stored
+    key                  = "website.terraform.tfstate"  # The state file name
+  }
+}
+
+
+provider "azurerm" {
+    subscription_id = "30ee9279-e76e-409d-8973-00c9792f6bcb"
+  features {
+
+  }
+}
---
+++ module06/staticwebsite/variables.tf
@@ -0,0 +1,29 @@
+variable "location" {
+    description = "The location of the resource group"
+    type        = string
+    default = "westeurope"
+}
+
+variable "rg_name" {
+    description = "The name of the resource group"
+    type        = string
+    default = "rg-web"
+}
+
+variable "sa_name" {
+    description = "The name of the storage account"
+    type        = string
+    default = "saweb"
+}
+
+variable "source_content" {
+    description = "The content of the index.html file"
+    type        = string
+    default = "<h1>Made with Terraform</h1>"
+}
+
+variable "index_document" {
+    description = "The name of the index document"
+    type        = string
+    default = "index.html"
+}
